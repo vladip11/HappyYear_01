2ef93bf7c0719ed09053667c82fb37870e87eb6d
vladip11 <yorp44444@gmail.com>
2020-07-07 12:08:31 -0400

    +instalacion_bootstrap_axios_router

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
    <!-- <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>-->
     <Cabecera></Cabecera>
+    <router-view></router-view>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -4,9 +4,24 @@ import 'jquery';
 import 'popper.js';
 import 'bootstrap';
 import './assets/app.css';
+import VueRouter from 'vue-router';
+import Cabecera from './components/Cabecera.vue';
+
 //loesa@tecmilenio.mx
 Vue.config.productionTip = false
 
+Vue.use(VueRouter);
+
+const routes = [
+  {path: '/home', component: Cabecera}
+];
+
+const router = new VueRouter({
+  routes,
+  mode: 'history'
+});
+
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
